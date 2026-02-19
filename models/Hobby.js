67ed1cc36394680e5dc93ad07d3c71b06439d2d4
anri gobadze <anrigobadze02@gmail.com>
2026-02-19 09:20:50 +0400

    Add all project files

--- models/Hobby.js
+++ models/Hobby.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-const hobbySchema = new mongoose.Schema({
-    name:{type:String, trim:true, max:50, required:[true, ["name is required"]]},
-    description:{type:String, trim:true, max:500},
-    category:{type:String, enum: sports, arts, music, gaming, reading, other},
-    frequency:{type:String, enum: daily, weekly, monthly, rarely},
-    isActive:{type:Boolean, default:true},
+const hobbySchema = new mongoose.Schema(
+  {
+    name: { type: String, trim: true, maxlength: 50, required: [true, "name is required"] },
+    description: { type: String, trim: true, maxlength: 500 },
+    category: { type: String, enum: ["sports", "arts", "music", "gaming", "reading", "other"] },
+    frequency: { type: String, enum: ["daily", "weekly", "monthly", "rarely"] },
+    isActive: { type: Boolean, default: true }
+  },
+  { timestamps: true }
+);
 
-},{timestamps})
-
-
-export default mongoose.module("Hobby", hobbySchema);
+export default mongoose.model("Hobby", hobbySchema);
--- routes/hobbyRoutes.js
+++ routes/hobbyRoutes.js
@@ -1,28 +1,81 @@
 import express from "express";
-import Hobby from "../models/Hobby.js";
+import mongoose from "mongoose";
+import Hobby from "../models/Hobby.js"
 
-const hobbyRouter = express.Router()
+
+
+const hobbyRouter = express.Router();
+
 
 hobbyRouter.post("/", async (req, res) => {
     try {
         const {name, description, category, frequency, isActive} = req.body
         if(!name){
-            res.status(400).json({message:"name is required"})
-        }
+            return res.status(400).json({message:"name is required"})
+        };
 
-        const createHobby = await Hobby.create({
+        const createdHobby = Hobby.create({
             name,
-            description, 
-            category, 
-            frequency, 
+            description,
+            category,
+            frequency,
             isActive
         })
 
-        res.status(201).json({message:`Created succesfuly: ${createHobby}`});
+        res.status(201).json("created succesfuly", createdHobby)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({message: error.message})
     }
 })
 
 
 
+hobbyRouter.get("/", async (req, res) => {
+    try {
+        const allHobies = await Hobby.find();
+        res.status(400).json(allHobies)
+    } catch (error) {
+        res.status(500).json({message: error.message})       
+    }
+})
+
+hobbyRouter.get("/:id", async (req,res) => {
+    try {
+        const {id} = req.params
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json("Invalid ID")
+        }
+
+        const hobbyByID = await Hobby.findById(id);
+        if(!hobbyByID){
+        return res.status(404).json("hobby not found")
+
+        }
+        res.status(500).json(hobbyByID)
+
+    } catch (error) {
+        res.status(500).json({message: error.message})        
+    }
+})
+
+
+
+hobbyRouter.delete("/:id", async (req,res) => {
+    try {
+        const {id} = req.params;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json("Invalid ID")  
+        }
+
+        const hobbyToDelete = await Hobby.findByIdAndDelete(id);
+        if(!hobbyToDelete){
+          return res.status(404).json("hobby not found")
+        }
+
+
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})        
+
+    }
+})
